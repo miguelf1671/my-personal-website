a2faf6fb706143f963f0862db5df12a4318aebba
miguel flores rammirez <deeznutz@miguels-Mini.home>
2024-01-19 09:51:18 -0500

    minor changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,64 @@ import React from "react";
 import "../App.css";
 
 function Home() {
+  const gradientColor = (
+    // setting this as a global variable to use for anything you want to make gradient
+    <defs>
+      {/* Define a linear gradient */}
+      <linearGradient id="myDesktopGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+        <stop offset="0%" style={{ stopColor: "#e26ee5" }} />
+        <stop offset="100%" style={{ stopColor: "#7e30e1" }} />
+      </linearGradient>
+    </defs>
+  );
+
+  const windowSvg = (
+    <svg
+      fill="#000000"
+      version="1.1"
+      id="desktop"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32.00 32.00"
+      width="70vw"
+      // height="auto"
+      stroke="#000000"
+      stroke-width="0.00032"
+    >
+      {gradientColor}
+      <g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          fill="url(#myDesktopGradient)"
+          id="desktop_1_"
+          d="M23,29.36H9v-0.72h6.64v-4.28H3c-1.301,0-2.36-1.059-2.36-2.36V5c0-1.301,1.059-2.36,2.36-2.36h26 c1.302,0,2.36,1.059,2.36,2.36v17c0,1.302-1.059,2.36-2.36,2.36H16.36v4.279H23V29.36z M1.36,19.36V22c0,0.904,0.736,1.64,1.64,1.64 h26c0.904,0,1.64-0.735,1.64-1.64v-2.64H1.36z M1.36,18.64h29.28V5c0-0.904-0.735-1.64-1.64-1.64H3C2.096,3.36,1.36,4.096,1.36,5 V18.64z"
+        />{" "}
+        <rect
+          id="_Transparent_Rectangle"
+          style={{ fill: "none" }}
+          width="32"
+          height="32"
+        />{" "}
+      </g>
+    </svg>
+  );
   return (
     <div class="background-picture">
       <div class="container">
-        <div class="home">
-          <h1>HELLO</h1>
-          <p>Nope</p>
+        <div class="description">
+          <div class="center-content">
+            <h1>MIGUEL FLORES</h1>
+            {windowSvg}
+          </div>
         </div>
+        <div id="projects"></div>
       </div>
     </div>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import "../App.css";
 
+const gradientColor = (
+  // setting this as a global variable to use for anything you want to make gradient
+  <defs>
+    {/* Define a linear gradient */}
+    <linearGradient id="mySvgGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+      <stop offset="0%" style={{ stopColor: "#e26ee5" }} />
+      <stop offset="100%" style={{ stopColor: "#7e30e1" }} />
+    </linearGradient>
+  </defs>
+);
+
 const burgerSvg = (
   <svg
     width="2rem"
@@ -9,13 +20,7 @@ const burgerSvg = (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <defs>
-      {/* Define a linear gradient */}
-      <linearGradient id="mySvgGradient" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" style={{ stopColor: "#e26ee5" }} />
-        <stop offset="100%" style={{ stopColor: "#7e30e1" }} />
-      </linearGradient>
-    </defs>
+    {gradientColor}
 
     <path
       fill="url(#mySvgGradient)"
@@ -46,19 +51,52 @@ const closeMenuIcon = (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <defs>
-      {/* Define a linear gradient */}
-      <linearGradient id="closeMenuGradient" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" style={{ stopColor: "#e26ee5" }} />
-        <stop offset="100%" style={{ stopColor: "#7e30e1" }} />
-      </linearGradient>
-    </defs>
-    <path
-      fill="url(#closeMenuGradient"
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z"
-    />
+    {gradientColor}
+
+    <g fill="url(#mySvgGradient)">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z"
+      />
+    </g>
+  </svg>
+);
+
+const linkedInIcon = (
+  <svg
+    width="2rem"
+    height="2rem"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="-143 145 512 512"
+  >
+    {gradientColor}
+    <g fill="url(#mySvgGradient)">
+      <path d="M-143,145v512h512V145H-143z M339,627h-452V175h452V627z" />
+      <rect x="-8.5" y="348.4" width="49.9" height="159.7" />
+      <path
+        d="M127.9,508.1v-86.3c0-4.9-0.2-9.7,1.2-13.1c3.8-9.6,12.1-19.6,27-19.6c19.5,0,28.3,14.8,28.3,36.4v82.6H241v-88.8
+      c0-49.4-27.8-72.4-63.3-72.4c-28.6,0-46.5,15.6-49.8,26.6v-25.1H71.8c0.7,13.3,0,159.7,0,159.7H127.9z"
+      />
+      <path d="M14.7,328.4h0.4c18.8,0,30.5-12.3,30.4-27.7C45.1,284.9,33.8,273,15.4,273c-18.4,0-30.5,11.9-30.5,27.7 C-15.1,316.2-3.4,328.4,14.7,328.4z" />
+    </g>
+  </svg>
+);
+
+const githubIcon = (
+  <svg
+    width="2rem"
+    height="2rem"
+    viewBox="0 0 192 192"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    {gradientColor}
+    <g fill="url(#mySvgGradient)">
+      <path
+        stroke-width="12"
+        d="M120.755 170c.03-4.669.059-20.874.059-27.29 0-9.272-3.167-15.339-6.719-18.41 22.051-2.464 45.201-10.863 45.201-49.067 0-10.855-3.824-19.735-10.175-26.683 1.017-2.516 4.413-12.63-.987-26.32 0 0-8.296-2.672-27.202 10.204-7.912-2.213-16.371-3.308-24.784-3.352-8.414.044-16.872 1.14-24.785 3.352C52.457 19.558 44.162 22.23 44.162 22.23c-5.4 13.69-2.004 23.804-.987 26.32C36.824 55.498 33 64.378 33 75.233c0 38.204 23.149 46.603 45.2 49.067-3.551 3.071-6.719 9.138-6.719 18.41 0 6.416.03 22.621.059 27.29M27 130c9.939.703 15.67 9.735 15.67 9.735 8.834 15.199 23.178 10.803 28.815 8.265"
+      />
+    </g>
   </svg>
 );
 
@@ -72,7 +110,7 @@ function Nav() {
   return (
     <header class="primary-header flex">
       <div>
-        <h1 class="logo">MIGUEL FLORES</h1>
+        <h1 class="logo">MF</h1>
       </div>
 
       <button
@@ -92,19 +130,26 @@ function Nav() {
           data-visible={isVisible.toString()}
           class={`primary-navigation flex ${isVisible ? "visible" : "hidden"}`}
         >
-          <li class="active">
+          <li>
             <a href="https://www.google.com/">
-              <span aria-hidden="true">00</span>Home
+              <span aria-hidden="true">00</span>About
             </a>
           </li>
           <li>
             <a href="https://www.google.com/">
-              <span aria-hidden="true">01</span>About
+              <span aria-hidden="true">01</span>Resume
             </a>
           </li>
           <li>
-            <a href="https://www.google.com/">
-              <span aria-hidden="true">02</span>Resume
+            <a class="icons" href="">
+              <span aria-hidden="true"></span>
+              {linkedInIcon}
+            </a>
+          </li>
+          <li>
+            <a class="icons" href="">
+              <span aria-hidden="true"></span>
+              {githubIcon}
             </a>
           </li>
         </ul>
