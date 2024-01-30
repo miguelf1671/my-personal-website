122f7af0603a904559243c3d15cd0f3621855aca
miguelf1671 <miguelf1671@gmail.com>
2024-01-29 20:45:00 -0500

    version 1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,20 +6,18 @@ function Footer() {
   return (
     <footer>
       <div class="footer-container">
+        <div class="footer-nav">
+          <h2>Follow Here</h2>
+        </div>
         <div class="social-icons">
           <a href="">{LinkedinIcon()}</a>
           <a href="">{GithubIcon()}</a>
           <a href="">{InstagramIcon()}</a>
         </div>
-        <div class="footer-nav">
-          <ul>
-            <h2>Follow me!</h2>
-          </ul>
-        </div>
         <div class="footer-bottom">
           <p>
             Copyright &copy; 2024; Designed by
-            <span class="designer">Miguel Flores</span>
+            <span class="designer"> Miguel Flores</span>
           </p>
         </div>
       </div>
